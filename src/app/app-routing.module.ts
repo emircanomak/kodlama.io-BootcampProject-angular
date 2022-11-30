632222dd059aa9eeabbd070344984998bb76fb39
Beyza ALICI <alicibeyza6@gmail.com>
2022-11-30 23:35:17 +0300

    add applicant List

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
+import { CreateApplicantComponent } from './components/applicant/create-applicant/create-applicant.component';
 import { CreateBootcampComponent } from './components/bootcamp/create-bootcamp/create-bootcamp.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
 
-  {path:"bootcampList",component:CreateBootcampComponent}
+  {path:"bootcampList",component:CreateBootcampComponent},
+  {path:"applicantList",component:CreateApplicantComponent}
 
 ];
 
--- src/app/services/applicant.service.ts
+++ src/app/services/applicant.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ICreateApplicantModel } from '../models/request/applicant/createApplicantModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApplicantService {
+  apiUrl = "http://localhost:3000/applicant"
+  applicants : ICreateApplicantModel[] = []
+  constructor(private httpClient : HttpClient) { }
 
-  constructor() { }
+  getApplicant():Observable<ICreateApplicantModel[]>{
+
+    return this.httpClient.get<ICreateApplicantModel[]>(this.apiUrl)
+
+  }
 }

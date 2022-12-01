137887d39fc5ce4a617f8b3c9fc9194e6e118829
Berkay Anduv <sygrok@hotmail.com>
2022-12-01 10:38:22 +0300

    add-application

--- src/app/components/application/create-application/create-application.component.ts
+++ src/app/components/application/create-application/create-application.component.ts
@@ -1,3 +1,8 @@
+import { ApplicationService } from './../../../services/application.service';
+import { ICreateApplicationModel } from './../../../models/request/application/createApplicationModel';
+import { ICreateApplicantModel } from 'src/app/models/request/applicant/createApplicantModel';
+import { ActivatedRoute } from '@angular/router';
+import { ApplicantService } from './../../../services/applicant.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +11,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-application.component.css']
 })
 export class CreateApplicationComponent {
+  applications:ICreateApplicationModel[]=[]
+  constructor(private applicationService:ApplicationService, private activatedRoute:ActivatedRoute){}
+  ngOnInit(): void{
+    this.activatedRoute.params.subscribe(params=>{
+      this.getApplication();
+    })
+}
+
+  getApplication(){
+    this.applicationService.getApplication().subscribe(data => {
+      this.applications = data
+    })
+  }
+
+
 
 }
--- src/app/services/application.service.ts
+++ src/app/services/application.service.ts
@@ -1,9 +1,18 @@
+import { Observable } from 'rxjs';
+import { ICreateApplicationModel } from './../models/request/application/createApplicationModel';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApplicationService {
-  
-  constructor() { }
+  applicationModel:ICreateApplicationModel[]=[]
+  path="http://localhost:3000/application"
+  constructor(private httpClient:HttpClient) { }
+
+  getApplication():Observable<ICreateApplicationModel[]>{
+   return this.httpClient.get<ICreateApplicationModel[]>(this.path)
+  }
+
 }

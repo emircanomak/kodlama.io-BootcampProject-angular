a6c2f0c97443041523ab9945f9793884e944b87b
Hediye Yildirim <hediyeyldrm0@gmail.com>
2022-12-01 17:08:32 +0300

    update function

--- src/app/components/instructor/update-instructor/update-instructor.component.ts
+++ src/app/components/instructor/update-instructor/update-instructor.component.ts
@@ -1,10 +1,55 @@
+import { ActivatedRoute } from '@angular/router';
+import { IUpdateInstructorModel } from './../../../models/request/instructor/updateInstructorModel';
+import { InstructorService } from './../../../services/instructor.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-update-instructor',
   templateUrl: './update-instructor.component.html',
-  styleUrls: ['./update-instructor.component.css']
+  styleUrls: ['./update-instructor.component.css'],
 })
 export class UpdateInstructorComponent {
+  instructorUpdateForm: FormGroup;
+  instructor: IUpdateInstructorModel;
 
+  constructor(
+    private instructorService: InstructorService,
+    private formBuilder: FormBuilder,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      this.getInstructorById(params['id']);
+    });
+  }
+
+  createInstructorUpdateForm() {
+    this.instructorUpdateForm = this.formBuilder.group({
+      firstName: [this.instructor.firstName, Validators.required],
+      lastName: [this.instructor.lastName, Validators.required],
+      email: [this.instructor.email, Validators.required],
+      password: [this.instructor.password, Validators.required],
+      nationalIdentity: [this.instructor.nationalIdentity, Validators.required],
+      dateOfBirth: [this.instructor.dateOfBirth, Validators.required],
+      companyName: [this.instructor.companyName, Validators.required],
+    });
+  }
+
+  getInstructorById(instructorId: number) {
+    this.instructorService.getInstructorById(instructorId).subscribe((data) => {
+      this.instructor = data;
+      this.createInstructorUpdateForm();
+    });
+  }
+
+  update(): void {
+    this.instructorService
+      .update(
+        this.activatedRoute.snapshot.params['id'],
+        this.instructorUpdateForm.value
+      )
+      .subscribe((data) => {});
+  }
 }

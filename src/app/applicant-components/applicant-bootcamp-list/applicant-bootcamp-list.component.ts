855be1c6e82b7240e2a23393436e4328e2e6947d
Hediye Yildirim <hediyeyldrm0@gmail.com>
2022-12-05 21:25:25 +0300

    bootcamp list

--- src/app/applicant-components/applicant-bootcamp-list/applicant-bootcamp-list.component.ts
+++ src/app/applicant-components/applicant-bootcamp-list/applicant-bootcamp-list.component.ts
@@ -1,10 +1,30 @@
+import { ActivatedRoute } from '@angular/router';
+import { BootcampService } from './../../services/bootcamp.service';
+import { ICreateBootcampModel } from './../../models/request/bootcamp/createBootcampModel';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-applicant-bootcamp-list',
   templateUrl: './applicant-bootcamp-list.component.html',
-  styleUrls: ['./applicant-bootcamp-list.component.css']
+  styleUrls: ['./applicant-bootcamp-list.component.css'],
 })
 export class ApplicantBootcampListComponent {
+  bootcamps: ICreateBootcampModel[] = [];
 
+  constructor(
+    private bootcampService: BootcampService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      this.getBootcamp();
+    });
+  }
+
+  getBootcamp() {
+    this.bootcampService
+      .getBootcamp()
+      .subscribe((data) => (this.bootcamps = data));
+  }
 }

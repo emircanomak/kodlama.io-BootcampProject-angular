ce1d442599046da8b8f8f938b08ae4fe8d784fdc
Berkay Anduv <sygrok@hotmail.com>
2022-12-05 14:54:06 +0300

    add-instructor--component

---
+++ src/app/instructor-components/instructor-profile/instructor-profile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-instructor-profile',
+  templateUrl: './instructor-profile.component.html',
+  styleUrls: ['./instructor-profile.component.css']
+})
+export class InstructorProfileComponent {
+
+}

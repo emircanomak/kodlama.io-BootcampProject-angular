5120e0fd7ea6ae8d071daf81f0c61876345f5b86
Hediye Yildirim <hediyeyldrm0@gmail.com>
2022-12-05 17:17:32 +0300

    edit applicant components

--- src/app/applicant-components/applicant-instructor-list/applicant-instructor-list.component.ts
+++ src/app/applicant-components/applicant-instructor-list/applicant-instructor-list.component.ts
@@ -1,10 +1,30 @@
+import { ActivatedRoute } from '@angular/router';
+import { InstructorService } from './../../services/instructor.service';
+import { ICreateInstructorModel } from './../../models/request/instructor/createInstructorModel';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-applicant-instructor-list',
   templateUrl: './applicant-instructor-list.component.html',
-  styleUrls: ['./applicant-instructor-list.component.css']
+  styleUrls: ['./applicant-instructor-list.component.css'],
 })
 export class ApplicantInstructorListComponent {
+  instructors: ICreateInstructorModel[] = [];
 
+  constructor(
+    private instructorService: InstructorService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      this.getInstructor();
+    });
+  }
+
+  getInstructor() {
+    this.instructorService
+      .getInstructor()
+      .subscribe((data) => (this.instructors = data));
+  }
 }

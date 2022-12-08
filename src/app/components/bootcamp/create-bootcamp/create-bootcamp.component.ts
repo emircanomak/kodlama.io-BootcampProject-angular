a680f14be2cfb515674d03cd6fad5b0e52f358fe
Berkay Anduv <sygrok@hotmail.com>
2022-12-08 19:19:27 +0300

    add-modal

--- src/app/components/bootcamp/create-bootcamp/create-bootcamp.component.ts
+++ src/app/components/bootcamp/create-bootcamp/create-bootcamp.component.ts
@@ -1,3 +1,4 @@
+import Swal  from 'sweetalert2';
 import { BootcampService } from './../../../services/bootcamp.service';
 import { Component } from '@angular/core';
 import { ICreateBootcampModel } from 'src/app/models/request/bootcamp/createBootcampModel';
@@ -26,9 +27,51 @@ export class CreateBootcampComponent {
       this.bootcampService.getBootcamp().subscribe(data=> this.bootcamps=data)
 
     }
-    delete(bootcamp:ICreateBootcampModel){
-      this.bootcamps = this.bootcamps.filter(b=>b!==bootcamp);
-      this.bootcampService.delete(bootcamp).subscribe()
-      this.toastrService.error("Silindi")
+    // delete(bootcamp:ICreateBootcampModel){
+    //   this.bootcamps = this.bootcamps.filter(b=>b!==bootcamp);
+    //   this.bootcampService.delete(bootcamp).subscribe()
+    //   this.toastrService.error("Silindi")
+    // }
+
+    delete(id:number){
+      const swalWithBootstrapButtons=Swal.mixin({
+        customClass:{
+          confirmButton:'btn bg-gradient-info active ms-3',
+          cancelButton:'btn bg-gradient-primary active'
+        },
+        buttonsStyling:false,
+      
+      });
+      swalWithBootstrapButtons.fire({
+        title:'Emin misiniz',
+        text:'Bu işlem geri alınmaz',
+        icon:'warning',
+        showCancelButton:true,
+        confirmButtonText:'Evet',
+        cancelButtonText:'Hayır',
+        reverseButtons:true,
+    
+      })
+      .then((result)=>{
+        if(result.isConfirmed){
+          this.bootcampService.delete(id).subscribe(()=>{
+            swalWithBootstrapButtons.fire(
+              'Silindi',
+              'İstediğiniz veri silme işlemi başarılı',
+              'success',
+            );
+          }),
+          setTimeout(()=>{
+            window.location.reload();
+          },1000);
+        }else if(result.dismiss===Swal.DismissReason.cancel){
+          swalWithBootstrapButtons.fire(
+            'İptal Edildi',
+            'Veriniz güvende',
+            'error',
+          );
+
+        }
+      })
     }
 }
